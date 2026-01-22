b52c6b0e4d3ff10582c2f4f562285f3e353d419c
Jonathan <bernardojonathansimon@gmail.com>
2026-01-22 13:30:20 +0800

    updated urdf code

--- src/chassis/setup.py
+++ src/chassis/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
         (os.path.join('share', package_name, 'description'), glob('description/*')),
+        (os.path.join('share', package_name, 'description_old'), glob('description_old/*')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
         (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
