2964ac056f99006ac9695601ead8c6cefdb79d30
jsdgbernardo <bernardojonathansimon@gmail.com>
2026-02-26 16:40:07 +0800

    collision avoidance logging

--- src/collision_avoidance/src/avoidance_planner.cpp
+++ src/collision_avoidance/src/avoidance_planner.cpp
@@ -3,7 +3,8 @@
 AvoidancePlanner::AvoidancePlanner(float safe_dist, float slow_dist,
                                    float base_speed, float turn_speed)
   : safe_dist_(safe_dist), slow_dist_(slow_dist),
-    base_speed_(base_speed), turn_speed_(turn_speed)
+    base_speed_(base_speed), turn_speed_(turn_speed),
+    last_state_("CLEAR")
 {
 }
 
@@ -18,10 +19,14 @@ geometry_msgs::msg::Twist AvoidancePlanner::compute_cmd(
   // TO DO: account for coordanates of the shelves/permanent items in the map
   // TO DO: finetune safe distance/speed parameters based on irl dimensions
 
+  std::string state;
+
   if (front < safe_dist_) { // CA-3
     // STOP + TURN
     cmd.linear.x = 0.0;
     cmd.angular.z = (left > right) ? turn_speed_ : -turn_speed_; // turn towards the direction with more space
+
+    state = "AVOIDING";
 
     // TO DO: needs to be finetuned to curve around the obstacle but ultimately return to global path
   }
@@ -29,6 +34,8 @@ geometry_msgs::msg::Twist AvoidancePlanner::compute_cmd(
     // SLOW + TURN
     cmd.linear.x = base_speed_ * 0.5;
     cmd.angular.z = (left > right) ? turn_speed_ * 0.5 : -turn_speed_ * 0.5;
+
+    state = "AVOIDING";
 
     // TO DO: needs to return back to global path
   }
@@ -39,7 +46,63 @@ geometry_msgs::msg::Twist AvoidancePlanner::compute_cmd(
     // TO DO: subscribe to global path
     cmd.linear.x = base_speed_;
     cmd.angular.z = 0.0;
+
+    state = "CLEAR";
+  }
+
+  // Log state only if it changed
+  if (state != last_state_) {
+    RCLCPP_INFO(rclcpp::get_logger("avoidance_planner"), "State: %s", state.c_str());
+    last_state_ = state;
   }
 
   return cmd;
 }
+
+
+
+// #include "collision_avoidance/avoidance_planner.hpp"
+
+// AvoidancePlanner::AvoidancePlanner(float safe_dist, float slow_dist,
+//                                    float base_speed, float turn_speed)
+//   : safe_dist_(safe_dist), slow_dist_(slow_dist),
+//     base_speed_(base_speed), turn_speed_(turn_speed)
+// {
+// }
+
+// geometry_msgs::msg::Twist AvoidancePlanner::compute_cmd(
+//   float front, float left, float right)
+// {
+//   geometry_msgs::msg::Twist cmd;
+
+//   // dynammic obstacles will follow the same
+//   // limitation: dynamic obstacles moving too fast
+
+//   // TO DO: account for coordanates of the shelves/permanent items in the map
+//   // TO DO: finetune safe distance/speed parameters based on irl dimensions
+
+//   if (front < safe_dist_) { // CA-3
+//     // STOP + TURN
+//     cmd.linear.x = 0.0;
+//     cmd.angular.z = (left > right) ? turn_speed_ : -turn_speed_; // turn towards the direction with more space
+
+//     // TO DO: needs to be finetuned to curve around the obstacle but ultimately return to global path
+//   }
+//   else if (front < slow_dist_) { // CA-1, CA-2
+//     // SLOW + TURN
+//     cmd.linear.x = base_speed_ * 0.5;
+//     cmd.angular.z = (left > right) ? turn_speed_ * 0.5 : -turn_speed_ * 0.5;
+
+//     // TO DO: needs to return back to global path
+//   }
+//   else {
+//     // CLEAR PATH
+
+//     // directions will be taken from global planner
+//     // TO DO: subscribe to global path
+//     cmd.linear.x = base_speed_;
+//     cmd.angular.z = 0.0;
+//   }
+
+//   return cmd;
+// }
--- src/collision_avoidance/src/collision_avoidance_node.cpp
+++ src/collision_avoidance/src/collision_avoidance_node.cpp
@@ -1,4 +1,5 @@
 #include "collision_avoidance/collision_avoidance_node.hpp"
+#include <std_msgs/msg/string.hpp> 
 
 CollisionAvoidanceNode::CollisionAvoidanceNode()
 : Node("collision_avoidance_node"),
@@ -22,9 +23,12 @@ CollisionAvoidanceNode::CollisionAvoidanceNode()
     std::bind(&CollisionAvoidanceNode::cmd_callback, this, std::placeholders::_1)
   );
 
-  // Output publisher
+  // Output publishers
   cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
     "/cmd_vel_collision", 10);
+
+  state_pub_ = this->create_publisher<std_msgs::msg::String>(
+    "/cart_state", 10);   // NEW
 
   RCLCPP_INFO(this->get_logger(), "Collision Avoidance Node started");
 }
@@ -58,7 +62,22 @@ void CollisionAvoidanceNode::cmd_callback(
       detector_.right_distance()
     );
 
+    std::string state = (detector_.front_distance() < planner_.get_safe_dist()) ? "AVOIDING" : "CLEAR";
+
+    if (state != last_state_) {
+      RCLCPP_INFO(this->get_logger(), "State: %s", state.c_str());
+      last_state_ = state;
+    }
+
     RCLCPP_WARN(this->get_logger(), "Obstacle detected! Avoiding...");
+  }
+  else
+  {
+    std::string state = "CLEAR";
+    if (state != last_state_) {
+      RCLCPP_INFO(this->get_logger(), "State: %s", state.c_str());
+      last_state_ = state;
+    }
   }
 
   cmd_pub_->publish(safe_cmd);
@@ -77,6 +96,7 @@ int main(int argc, char * argv[])
 
 
 
+
 // #include "collision_avoidance/collision_avoidance_node.hpp"
 
 // CollisionAvoidanceNode::CollisionAvoidanceNode()
@@ -89,16 +109,21 @@ int main(int argc, char * argv[])
 //     this->declare_parameter("turn_speed", 0.8)
 //   )
 // {
+//   // LiDAR subscription
 //   scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
 //     "/scan", 10,
 //     std::bind(&CollisionAvoidanceNode::scan_callback, this, std::placeholders::_1)
 //   );
 
-//   // cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
-//   //   "/cmd_vel_safe", 10);
+//   // Manual velocity subscription
+//   cmd_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
+//     "/cmd_vel_manual", 10,
+//     std::bind(&CollisionAvoidanceNode::cmd_callback, this, std::placeholders::_1)
+//   );
+
+//   // Output publisher
 //   cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
 //     "/cmd_vel_collision", 10);
-
 
 //   RCLCPP_INFO(this->get_logger(), "Collision Avoidance Node started");
 // }
@@ -108,14 +133,36 @@ int main(int argc, char * argv[])
 // {
 //   detector_.update(*msg);
 
-//   auto cmd = planner_.compute_cmd(
-//     detector_.front_distance(),
-//     detector_.left_distance(),
-//     detector_.right_distance()
-//   );
-
-//   cmd_pub_->publish(cmd);
+//   // Update obstacle flag based on front distance
+//   if (detector_.front_distance() < 0.5)  // threshold
+//     obstacle_detected_ = true;
+//   else
+//     obstacle_detected_ = false;
+//     // obstacle_detected_ = true; // for testing
 // }
+
+// void CollisionAvoidanceNode::cmd_callback(
+//   const geometry_msgs::msg::Twist::SharedPtr msg)
+// {
+//   last_cmd_ = *msg;
+
+//   geometry_msgs::msg::Twist safe_cmd = last_cmd_;
+
+//   if (obstacle_detected_)
+//   {
+//     // Use planner to generate avoidance command
+//     safe_cmd = planner_.compute_cmd(
+//       detector_.front_distance(),
+//       detector_.left_distance(),
+//       detector_.right_distance()
+//     );
+
+//     RCLCPP_WARN(this->get_logger(), "Obstacle detected! Avoiding...");
+//   }
+
+//   cmd_pub_->publish(safe_cmd);
+// }
+
 // int main(int argc, char * argv[])
 // {
 //   rclcpp::init(argc, argv);
